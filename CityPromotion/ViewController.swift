cdfefd04ff683b9c02e98aa1d0e0c4f109fcd928
hasanlaska <hasanhazirbulan@gmail.com>
2023-08-09 11:08:58 +0300

    Initial Commit

---
+++ CityPromotion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityPromotion
+//
+//  Created by Hasan Hazırbulan on 9.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
