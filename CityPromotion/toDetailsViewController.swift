0c81a5a49233f27aa93769ff401a30b56512d16b
hasanlaska <hasanhazirbulan@gmail.com>
2023-08-13 19:34:05 +0300

    first commit

---
+++ CityPromotion/toDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  toDetailsViewController.swift
+//  CityPromotion
+//
+//  Created by Hasan Hazırbulan on 9.08.2023.
+//
+
+import UIKit
+
+class toDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var sehirIsmiLabel: UILabel!
+    
+    @IBOutlet weak var sehirBolgesiLabel: UILabel!
+    
+    var secilenSehir : Sehir?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sehirIsmiLabel.text = secilenSehir?.isim
+        sehirBolgesiLabel.text = secilenSehir?.ulke
+        imageView.image = secilenSehir?.gorsel
+        
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+}
