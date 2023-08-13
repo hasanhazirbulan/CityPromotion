dd145e1e8a4753c69f0019c4fb79325b0f0c049c
hasanlaska <hasanhazirbulan@gmail.com>
2023-08-13 19:34:39 +0300

    last commit

--- CityPromotion/ViewController.swift
+++ CityPromotion/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sehirDizisi = [Sehir]()
+    var kullaniciSecimi : Sehir?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // sehirler
+        
+        let newyork = Sehir(isim: "New York", ulke: "United States", gorsel: UIImage(named: "new york")!)
+        
+        let tokyo = Sehir(isim: "Tokyo", ulke: "Japan", gorsel: UIImage(named: "tokyo")!)
+        
+        let buenosaires = Sehir(isim: "Buenos Aires", ulke: "Argentina", gorsel: UIImage(named: "buenos aires")!)
+        
+        let losangeles = Sehir(isim: "Los Angeles", ulke: "United States", gorsel: UIImage(named: "los angeles")!)
+        
+        let london = Sehir(isim: "London", ulke: "England", gorsel: UIImage(named: "london")!)
+        
+        sehirDizisi = [newyork, tokyo, buenosaires, losangeles, london]
+        
     }
-
-
-}
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return sehirDizisi.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = UITableViewCell()
+            cell.textLabel?.text = sehirDizisi[indexPath.row].isim
+            return cell
+        
+        }
+        
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        kullaniciSecimi = sehirDizisi[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! toDetailsViewController
+            destinationVC.secilenSehir = kullaniciSecimi
+            
+        }
+            
+    }
+    
+    }
 
---
+++ CityPromotion/sehir.swift
@@ -0,0 +1,25 @@
+//
+//  sehir.swift
+//  CityPromotion
+//
+//  Created by Hasan Hazırbulan on 9.08.2023.
+//
+import UIKit
+import Foundation
+
+class Sehir {
+    
+    
+    var isim : String
+    var ulke : String
+    var gorsel : UIImage
+    
+    init(isim: String, ulke: String, gorsel: UIImage) {
+        self.isim = isim
+        self.ulke = ulke
+        self.gorsel = gorsel
+    }
+    
+    
+    
+}
